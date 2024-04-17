104b80c50c1fcd52c4faa8a9df5171d1c59a7703
Maciej Boguś <92251786+maciejbogus@users.noreply.github.com>
2024-04-17 14:42:24 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+      alert("Geolocation is not supported by this browser.");
+    }
+}
